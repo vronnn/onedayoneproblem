976e0a88535e5931513d410ab843d3a8e2a3ab5e
vronnn <zhafrandzaky@gmail.com>
2023-04-21 01:48:17 +0700

    add a problem

---
+++ 8_codeforces_1722A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,a,b;
+    cin >> t;
+    char c;
+    while(t--) {
+        cin >> a >> c >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
