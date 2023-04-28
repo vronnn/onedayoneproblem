3a3be944dda91c02561df47b5897b4ab42b464f5
vronnn <zhafrandzaky@gmail.com>
2023-04-29 02:47:33 +0700

    add a problem

---
+++ 10_codeforces_791A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c = 0;
+    cin >> a >> b;
+    while(b >= a) {
+        a = a * 3;
+        b = b * 2;
+        c++;
+    }
+    cout << c;
+    return 0;
+}
